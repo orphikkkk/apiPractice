1c6e1ce30ed608f5dccaa65e29d0801bbf7e5e64
orphikkkk <sajag@live.com>
2022-05-07 13:31:55 +0545

    initial setup and migration

---
+++ database/migrations/2022_05_07_071057_create_companies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->integer('id',true)->unique();
+            $table->integer('category_id')->nullable();
+            $table->string('title',255);
+            $table->string('image',255)->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('status');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('company_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
